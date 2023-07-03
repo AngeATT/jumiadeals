456ffdaf63cfb5ab58262983517e5feaa4286dd1
Attoungbre Ange François <angeattoungbre21@gmail.com>
2023-07-03 12:57:05 +0000

    feat : gestion des erreurs d'authentification sur la page de login

--- src/app/modules/core/auth/auth.component.ts
+++ src/app/modules/core/auth/auth.component.ts
@@ -9,19 +9,52 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent {
-  errorMsg? = '';
+  isLoginCorrect : undefined | boolean;
+  isSubmited : boolean = false;
+  isEmailValid: boolean | undefined = true;
+  isPasswordValid : boolean | undefined = true;
+  errorMsg?= '';
+
   utilisateurForm = this.fb.group({
-    email: ['',[Validators.required, Validators.email]],
-    password: ['',[Validators.required,Validators.minLength(4),Validators.maxLength(12)]]
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(12)]]
   });
 
-  constructor(private router: Router, auth: AuthService, private fb: FormBuilder){
+
+
+  constructor(private router: Router, auth: AuthService, private fb: FormBuilder) {
+  }
+  authenticate(): void {
+    this.isSubmited = true;
+    if (!this.utilisateurForm.valid) {
+      this.isEmailValid = this.emailValid()
+      this.isPasswordValid = this.passwordValid();
+      this.isLoginCorrect = undefined;
+    }else{
+      //logic du back si le gar se connecte good on le renvoie sur 
+      this.router.navigateByUrl("/");
+      if (true){
+        this.isLoginCorrect = true;
+      }else{
+        this.isLoginCorrect = false;
+      }
+    }
+    console.log(this.utilisateurForm.get('email')?.valid);
   }
 
-  authenticate(): void{
-    if(!this.utilisateurForm.valid){
-
+  emailValid() : boolean | undefined{
+    if (this.isSubmited){
+      return !this.utilisateurForm.get('email')?.valid;
+    }else{
+      return false;
     }
-    console.log(this.utilisateurForm.valid);
+  }
+
+  passwordValid() : boolean | undefined{
+    if (this.isSubmited){
+      return !this.utilisateurForm.get('password')?.valid;
+    }else{
+      return false;
+    }
   }
 }
--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -6,11 +6,12 @@ import { MatIcon, MatIconModule } from "@angular/material/icon";
 import { AuthService } from "./services/auth.service";
 import { AuthComponent } from "./auth/auth.component";
 import { FormBuilder, ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
 
 @NgModule({
     declarations:[FooterComponent,HeaderComponent,AuthComponent],
     exports:[FooterComponent,HeaderComponent,AuthComponent],
-    imports:[MaterialFeatures, MatIconModule, ReactiveFormsModule],
+    imports:[CommonModule, MaterialFeatures, MatIconModule, ReactiveFormsModule],
     providers:[AuthService]
 })
 export class CoreModule{}
