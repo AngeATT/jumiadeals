3ea32d5e6f65f8ea53a07cb9ef677077b3a8d23a
Attoungbre Ange François <angeattoungbre21@gmail.com>
2023-07-13 08:45:45 +0000

    feat : ajout du service de sauvegarde des informations de l'utilisateurs

---
+++ src/app/modules/core/services/storage.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from "@angular/core";
+import { UserCredentials } from "./user.credentials";
+
+const USER_KEY = "auth-user";
+/**
+ * Classe pour gérer les informations de 
+ * connexion de l'utilisateur en mémoire
+ */
+@Injectable({
+    providedIn: 'root'
+})
+export class StorageService {
+    constructor() { }
+    clean(): void {
+        window.sessionStorage.clear();
+    }
+    /**
+     * 
+     * @param user identifiants de  
+     * l'utilisateur
+     */
+    public saveUser(user: UserCredentials): void {
+        window.sessionStorage.removeItem(USER_KEY);
+        window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
+    }
+
+    public getUser(): any {
+        const user = window.sessionStorage.getItem(USER_KEY);
+        if (user) {
+            return JSON.parse(user);
+        }
+        return {};
+    }
+
+    public isLoggedIn(): boolean {
+        const user = window.sessionStorage.getItem(USER_KEY);
+
+        if (user) {
+            return true;
+        }
+        return false;
+    }
+}
