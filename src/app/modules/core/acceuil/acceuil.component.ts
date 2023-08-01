f116dbf58090838190c9e7817e1fd1be0e83d294
Attoungbre Ange François <angeattoungbre21@gmail.com>
2023-08-01 16:08:37 +0000

    ajustement de la taille des conteneurs

--- src/app/modules/core/acceuil/acceuil.component.ts
+++ src/app/modules/core/acceuil/acceuil.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-acceuil',
   templateUrl: './acceuil.component.html',
-  styleUrls: ['./acceuil.component.css']
+  styleUrls: ['./acceuil.component.css','../shared/compteCSS.css']
 })
 export class AcceuilComponent {
   villes: String[] = ["Abidjan", "Bouake", "Daloa", "Korogho", "San-pedro", "Yamoussoukro"];
